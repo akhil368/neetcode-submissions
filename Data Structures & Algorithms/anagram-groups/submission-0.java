2cd418de40a15d0aa606020703677eea779e5144
Akhil Suryan <77136397+akhil368@users.noreply.github.com>
2026-04-20 10:47:32 +0530

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,ArrayList<String>> map=new HashMap<>();
+
+        for(String s:strs)
+        {
+            char[] arr=s.toCharArray();
+            Arrays.sort(arr);
+            String str=new String(arr);
+
+            if(!map.containsKey(str))
+            {
+                map.put(str,new ArrayList<>());
+            }
+            map.get(str).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
